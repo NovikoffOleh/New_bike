a68e9748839f0990352d7ec8bec993cc4fc3aa3c
NovikoffOleh <novikoff10000@gmail.com>
2023-08-16 16:19:34 +0300

    upd files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function(){
-  $('.bike-slider').slick();
+  $('.bike-slider').slick({
+    arrows:false,
+    dots: true,
+    fade:true
+  });
 })
+//! отключение стрелок у слайдера
+//arrows:false,
+//dots: true
